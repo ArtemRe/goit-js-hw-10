d59457ccf1477f497ad85b04a431b335c6cd6a67
ArtemRe <RechembeiArtem@gmail.com>
2022-08-10 20:17:28 +0300

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-// import generatorLis from './genLis';
-// import generatorCard from './genCard';
-
-// import API from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ApiCountries from './fetchCountries';
 const debounce = require('lodash.debounce');
@@ -14,26 +10,27 @@ const refs = {
 
 refs.input.addEventListener(
   'input',
-  debounce(e => contriesAdd(e.target.value), 1000)
+  debounce(e => contriesAdd(e.target.value), 300)
 );
 function contriesAdd(text) {
   const countriesHandler = new ApiCountries();
   countriesHandler.input = text.trim();
   let contries = countriesHandler.searchCountry();
   contries.then(data => {
-    console.log(data);
+    // console.log(data);
     if (!data.length) {
       return Notify.failure('Oops, there is no country with that name');
     }
 
     if (data.length > 10) {
-      return Notify.failure('to match', {
-        useIcon: false,
-      });
+      return Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
     }
 
     createContry(data);
   });
+  resetHTML();
 }
 
 function createContry(data) {
@@ -48,22 +45,32 @@ function createContry(data) {
     span.innerHTML = element.name.official;
     img = document.createElement('img');
     img.setAttribute('src', element.flags.svg);
-    img.setAttribute('height', '30px');
+    img.setAttribute('height', '60px');
     img.setAttribute('width', '60px');
     li.append(img);
     li.append(span);
     ul.append(li);
+
     if (data.length === 1) {
+      img.classList.add('img-big');
+      span.style.fontSize = '40px';
       population = document.createElement('li');
       capital = document.createElement('li');
       languages = document.createElement('li');
-      population.innerHTML = element.population;
-      capital.innerHTML = element.capital[0];
-      languages.innerHTML = Object.values(element.languages).join(', ');
+      population.innerHTML = `Population: ${element.population}`;
+      capital.innerHTML = `Capital: ${element.capital[0]}`;
+      languages.innerHTML = `Languages: ${Object.values(element.languages).join(
+        ', '
+      )}`;
       console.log(Object.values(element.languages));
       ul.append(capital);
       ul.append(population);
       ul.append(languages);
     }
   });
+}
+
+function resetHTML(er) {
+  refs.card.innerHTML = '';
+  refs.ul.innerHTML = '';
 }
